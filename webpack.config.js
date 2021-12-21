eedca9ff567e8d1020abeb2bbc925c6e46941e17
Timo K <toger5@hotmail.de>
2021-12-21 03:04:52 +0100

    add endpoint 0.2.1

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,13 @@ const VERSION_OUTPUTS = {
     v0_1_8: 'dist/0.1.8',
     v0_1_9: 'dist/0.1.9',
     v0_2_0: 'dist/0.2.0',
+    v0_2_1: 'dist/0.2.1',
 }
 module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, VERSION_OUTPUTS.vCurrent),
+        path: path.resolve(__dirname, VERSION_OUTPUTS.v0_2_1),
     },
     plugins: [
         new webpack.DefinePlugin({
